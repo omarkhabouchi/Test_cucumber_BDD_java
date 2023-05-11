3be9c5e7432816dd3296aa277c0b01f235d91865
omarkhabouchi <omarkhabouchi01@gmail.com>
2023-05-11 23:18:43 +0100

    initial commit

---
+++ src/test/java/com/run/run.java
@@ -0,0 +1,15 @@
+package com.run;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/features",glue= "{fet.feature}")
+
+public class run {
+	
+	
+}
